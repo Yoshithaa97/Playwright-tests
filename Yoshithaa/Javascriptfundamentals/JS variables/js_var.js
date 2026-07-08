34ead615f795d6dde90362173aab20f09b37fcfe
yoshithaa97 <yoshithaayadhav@gmail.com>
2026-07-08 14:11:46 +0530

    initial commit with json package

---
+++ Yoshithaa/Javascriptfundamentals/JS variables/js_var.js
@@ -0,0 +1,27 @@
+var num = 9;
+console.log(num);
+
+
+/*there are 3 types of variables in js 
+//var,let,const
+//var : it can be redeclared and updated and it has function scope
+//let : it can be updated but not redeclared and it has block scope
+//const: it cannot be updated and redeclared and it has block scope
+*/
+
+//var
+var name = "yoshithaa";
+var name = "chandrika";
+console.log(name);
+//let
+let num1=10;
+let num2 = 20;
+console.log(num1);
+//const
+const country="india";
+const foreigncountry ="germany";
+console.log(country);
+console.log(foreigncountry);
+
+
+
---
+++ Yoshithaa/Javascriptfundamentals/firstscript.js
@@ -0,0 +1,3 @@
+let num = 10;
+console.log(num);
+ console.log("hello world i am learning javascript");
